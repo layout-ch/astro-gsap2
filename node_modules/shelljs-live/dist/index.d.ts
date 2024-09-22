adcab421145c98d98b61955983f79de06ebed996
Layout.ch <164497867+layout-ch@users.noreply.github.com>
2024-09-22 18:50:23 +0200

    Delete node_modules directory

--- node_modules/shelljs-live/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Options, Callback } from './utils';
-declare function live(command: string | string[], options?: Options): number | null;
-declare function live(command: string | string[], callback: Callback): number | null;
-declare function live(command: string | string[], options: Options | undefined, callback: Callback): number | null;
-export = live;
